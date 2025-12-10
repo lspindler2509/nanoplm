a734ad2be70858c7e891b6ed3473a8f0d5fead1d
lspindler2509 <lisa.spindler@hotmail.de>
2025-12-10 11:46:13 +0100

    use other attention version

--- src/nanoplm/pretraining/models/triangular_model/attention.py
+++ src/nanoplm/pretraining/models/triangular_model/attention.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import torch
 from einops.layers.torch import Rearrange
 from torch import Tensor, nn
@@ -11,10 +13,10 @@ class AttentionPairBias(nn.Module):
     def __init__(
         self,
         c_s: int,
-        c_z: int,
-        num_heads: int,
+        c_z: Optional[int] = None,
+        num_heads: Optional[int] = None,
         inf: float = 1e6,
-        initial_norm: bool = True,
+        compute_pair_bias: bool = True,
     ) -> None:
         """Initialize the attention pair bias layer.
 
@@ -28,8 +30,6 @@ class AttentionPairBias(nn.Module):
             The number of heads.
         inf : float, optional
             The inf value, by default 1e6
-        initial_norm: bool, optional
-            Whether to apply layer norm to the input, by default True
 
         """
         super().__init__()
@@ -41,20 +41,20 @@ class AttentionPairBias(nn.Module):
         self.head_dim = c_s // num_heads
         self.inf = inf
 
-        self.initial_norm = initial_norm
-        if self.initial_norm:
-            self.norm_s = nn.LayerNorm(c_s)
-
         self.proj_q = nn.Linear(c_s, c_s)
         self.proj_k = nn.Linear(c_s, c_s, bias=False)
         self.proj_v = nn.Linear(c_s, c_s, bias=False)
         self.proj_g = nn.Linear(c_s, c_s, bias=False)
 
-        self.proj_z = nn.Sequential(
-            nn.LayerNorm(c_z),
-            nn.Linear(c_z, num_heads, bias=False),
-            Rearrange("b ... h -> b h ..."),
-        )
+        self.compute_pair_bias = compute_pair_bias
+        if compute_pair_bias:
+            self.proj_z = nn.Sequential(
+                nn.LayerNorm(c_z),
+                nn.Linear(c_z, num_heads, bias=False),
+                Rearrange("b ... h -> b h ..."),
+            )
+        else:
+            self.proj_z = Rearrange("b ... h -> b h ...")
 
         self.proj_o = nn.Linear(c_s, c_s, bias=False)
         init.final_init_(self.proj_o.weight)
@@ -64,9 +64,8 @@ class AttentionPairBias(nn.Module):
         s: Tensor,
         z: Tensor,
         mask: Tensor,
+        k_in: Tensor,
         multiplicity: int = 1,
-        to_keys=None,
-        model_cache=None,
     ) -> Tensor:
         """Forward pass.
 
@@ -75,11 +74,9 @@ class AttentionPairBias(nn.Module):
         s : torch.Tensor
             The input sequence tensor (B, S, D)
         z : torch.Tensor
-            The input pairwise tensor (B, N, N, D)
+            The input pairwise tensor or bias (B, N, N, D)
         mask : torch.Tensor
-            The pairwise mask tensor (B, N)
-        multiplicity : int, optional
-            The diffusion batch size, by default 1
+            The pairwise mask tensor (B, N, N)
 
         Returns
         -------
@@ -89,42 +86,25 @@ class AttentionPairBias(nn.Module):
         """
         B = s.shape[0]
 
-        # Layer norms
-        if self.initial_norm:
-            s = self.norm_s(s)
-
-        if to_keys is not None:
-            k_in = to_keys(s)
-            mask = to_keys(mask.unsqueeze(-1)).squeeze(-1)
-        else:
-            k_in = s
-
         # Compute projections
         q = self.proj_q(s).view(B, -1, self.num_heads, self.head_dim)
         k = self.proj_k(k_in).view(B, -1, self.num_heads, self.head_dim)
         v = self.proj_v(k_in).view(B, -1, self.num_heads, self.head_dim)
 
-        # Caching z projection during diffusion roll-out
-        if model_cache is None or "z" not in model_cache:
-            z = self.proj_z(z)
-
-            if model_cache is not None:
-                model_cache["z"] = z
-        else:
-            z = model_cache["z"]
-        z = z.repeat_interleave(multiplicity, 0)
+        bias = self.proj_z(z)
+        bias = bias.repeat_interleave(multiplicity, 0)
 
         g = self.proj_g(s).sigmoid()
 
-        # Compute attention weights (all in bfloat16)
-        attn = torch.einsum("bihd,bjhd->bhij", q, k)
-        attn = attn / (self.head_dim**0.5) + z
-        # The pairwise mask tensor (B, N) is broadcasted to (B, 1, 1, N) and (B, H, N, N)
-        attn = attn + (1 - mask[:, None, None]) * -self.inf
-        attn = attn.softmax(dim=-1)
+        with torch.autocast("cuda", enabled=False):
+            # Compute attention weights
+            attn = torch.einsum("bihd,bjhd->bhij", q.float(), k.float())
+            attn = attn / (self.head_dim**0.5) + bias.float()
+            attn = attn + (1 - mask[:, None, None].float()) * -self.inf
+            attn = attn.softmax(dim=-1)
 
-        # Compute output
-        o = torch.einsum("bhij,bjhd->bihd", attn, v)
+            # Compute output
+            o = torch.einsum("bhij,bjhd->bihd", attn, v.float()).to(v.dtype)
         o = o.reshape(B, -1, self.c_s)
         o = self.proj_o(g * o)
 
